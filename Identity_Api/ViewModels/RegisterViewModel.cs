c64a5d72f31dc450d25821eaa5e4108342f313bc
Hasan İncel <h_incel@hotmail.com>
2024-10-20 01:24:56 +0300

    RegisterViewModel.cs

--- Identity_Api/ViewModels/RegisterViewModel.cs
+++ Identity_Api/ViewModels/RegisterViewModel.cs
@@ -5,6 +5,7 @@ namespace Identity_Api.ViewModels
     public class RegisterViewModel
     {
         [Required]
+        [EmailAddress]
         public string Email { get; set; }
         [Required]
         public string Password { get; set; }
